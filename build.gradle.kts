d456d7bf7bd8620a07ed8894e1594cbc1aafac2b
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-11-30 03:25:31 +0900

    Chore: Update Gradle Structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,7 @@
 plugins {
-    id("com.android.application").version(ProjectVersions.application).apply(false)
-    id("com.android.library").version(ProjectVersions.library).apply(false)
-    id("org.jetbrains.kotlin.android").version(ProjectVersions.kotlinAndroid).apply(false)
-    id("io.github.gradle-nexus.publish-plugin").version("1.1.0")
-}
-
-buildscript {
-    dependencies {
-        this.classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
-    }
-}
-
-apply {
-    this.from("scripts/publish-root.gradle")
+    //trick: for the same plugin versions in all sub-modules
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
+    alias(libs.plugins.kotlin.multiplatform).apply(false)
 }
